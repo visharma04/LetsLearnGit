227d154e48103e7240fa0ad1a30e44df248d93e4
Vishal Sharma <visharma2025@gmail.com>
2024-07-25 20:52:48 +0530

    added new function in it

--- Sum.java
+++ Sum.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 public class Sum {
 
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in); 
         System.out.println("hello world");
+        System.out.println("Enter a number");
+        int a= sc.nextInt();
+        System.out.println("Enter second number");
+        int b= sc.nextInt();
+        int c= (a+b);
+        System.out.println("The sum is:"+c);
+
     }
 }
